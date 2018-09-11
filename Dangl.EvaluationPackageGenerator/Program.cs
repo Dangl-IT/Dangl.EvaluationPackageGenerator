e4cfaec6ec7319595b3063dc9d086e23e6e1329d
Georg Dangl <georg@dangl.me>
2018-09-11 22:44:14 +0200

    Add project files.

---
+++ Dangl.EvaluationPackageGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dangl.EvaluationPackageGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
