6259f0bd836c30ccf8566d7486580873827b7912
Georg Dangl <georg@dangl.me>
2021-11-16 22:15:34 +0100

    Set include prerelease option to false by default

--- Dangl.EvaluationPackageGenerator/CommandLineOptions.cs
+++ Dangl.EvaluationPackageGenerator/CommandLineOptions.cs
@@ -16,7 +16,7 @@ namespace Dangl.EvaluationPackageGenerator
         [Option(HelpText = "If specified, this file will be placed in the generated package")]
         public string ReadmePath { get; set; }
 
-        [Option(Required = false, Default = true, HelpText = "Whether or not to download the latest prerelease versions or stable versions")]
+        [Option(Required = false, Default = false, HelpText = "Whether or not to download the latest prerelease versions or stable versions")]
         public bool IncludePrerelease { get; set; }
     }
 }
