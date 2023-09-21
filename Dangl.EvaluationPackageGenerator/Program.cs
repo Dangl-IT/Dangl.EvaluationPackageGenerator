57aed09c9f28ca87bbe72230a6bb2262505d44ab
Georg Dangl <georg@dangl.me>
2023-09-21 13:30:08 +0200

    Update console message

--- Dangl.EvaluationPackageGenerator/Program.cs
+++ Dangl.EvaluationPackageGenerator/Program.cs
@@ -12,7 +12,7 @@ namespace Dangl.EvaluationPackageGenerator
             Console.WriteLine("Support: info@dangl-it.com");
             Console.WriteLine("Dangl IT GmbH - www.dangl-it.com");
             Console.WriteLine($"Version {VersionInfo.Version}, Built: {VersionInfo.BuildDateUtc.ToLocalTime():dd.MM.yyyy HH:mm}");
-            Console.WriteLine("This tool downloads packages from the 'dangl-ava' MyGet feed and bundles them in an evaluation package");
+            Console.WriteLine("This tool downloads packages from the 'dangl-ava' feed and bundles them in an evaluation package");
 
             var parsedOptions = Parser.Default.ParseArguments<CommandLineOptions>(args);
 
